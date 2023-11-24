fcee998f7bc7401d67958240b6a9d67c3b54b053
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-11-24 15:16:36 +0000

    created changes to gimage to take parameters of what to search

--- gimages.py
+++ gimages.py
@@ -1,7 +1,3 @@
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# |r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 from google_images_search import GoogleImagesSearch
 from dotenv import load_dotenv
 import os
@@ -12,38 +8,56 @@ import os
 
 # load the API key and CX code from .env file
 if os.path.exists(".env"):
-  load_dotenv()
+    load_dotenv()
 else:
-  print(".env file missing, please create one with your API and CX")
+    print(".env file missing, please create one with your API and CX")
 
 # create an 'ims' sub directory if it doesn't already exists
 if not os.path.exists('images/'):
-  os.mkdir('images/')
+    os.mkdir('images/')
 spath = 'images/'
 
 # Get env variables
 DK = os.environ.get('DEVELOPER_KEY')
 CX = os.environ.get('CX')
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'vision-ai-demo-402815-3045b2bd90da.json'
 
 # custom progressbar function
+
+
 def my_progressbar(url, progress):
     print(url + " " + progress + "%")
+
 
 # create google images search - object
 gis = GoogleImagesSearch(DK, CX, progressbar_fn=my_progressbar)
 
-def fetch_images(searchfor):
-  # using contextual mode (Curses)
-  with GoogleImagesSearch(DK, CX) as gis:
-    # define search params:
-    _search_params = {"q": searchfor, 
-        "num": 15, 
-        "safe": "high", 
-        "fileType": "jpg",
-        "imgType": "photo",
-        "rights": "cc_publicdomain" 
-        #  free for use by anyone for any purpose without restriction under copyright law
-        }
-    gis.search(search_params=_search_params, path_to_dir=spath)
 
-  print("Finished!")
+def fetch_images(searchfor):
+    # using contextual mode (Curses)
+    try:
+        with GoogleImagesSearch(DK, CX) as gis:
+            # define search params:
+            _search_params = {"q": searchfor,
+                              "num": 15,
+                              "safe": "high",
+                              "fileType": "jpg",
+                              "imgType": "photo",
+                              "rights": "cc_publicdomain"
+                              #  free for use by anyone for any purpose without restriction under copyright law
+                              }
+            gis.search(search_params=_search_params, path_to_dir=spath)
+    except:
+        print('failed to get this image')
+
+    print("Finished!")
+
+
+while (True):
+    q = input('image:>')
+    if q == 'c':
+        break
+    print('looking for:', q)
+    fetch_images(q)
+
+print('ciao!')
